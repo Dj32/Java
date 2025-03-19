c11c020da12084e4a39dcedbab651ad75ac83fa0
DJ <darienbooker23@gmail.com>
2025-03-19 15:18:04 -0400

    Workin' 19MAR25

---
+++ Java Book/Chapter 1/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        //Display method to print out "Welcome to Java"
+        System.out.println("Welcome to Java");
+    }
+}
