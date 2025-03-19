9876c6e31cac76e3ac880d9f4b2481fbdf556aaa
DJ <darienbooker23@gmail.com>
2025-03-19 15:51:43 -0400

    CH 1 Excercises

---
+++ Java Book/Chapter 1/Exercises/DisplayFiveMessages.java
@@ -0,0 +1,16 @@
+/*
+ * Write a program that displays Welcome to Java five times.
+ * DJ Booker
+ * 19 MAR 2025
+ */
+
+public class DisplayFiveMessages {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
---
+++ Java Book/Chapter 1/Exercises/DisplayThreeMessages.java
@@ -0,0 +1,16 @@
+/*
+ * Write a program that displays Welcome to Java,
+ * Welcome to Computer Science, and Programming is fun.
+ * 
+ * DJ Booker
+ * 19 MAR 2025
+ */
+
+public class DisplayThreeMessages {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
